9d206e38649e3dabb431b8bc049c50720e12c51b
DESKTOP-C01HDPH\Xavi <xavicsilvente@gmail.com>
2022-02-04 11:12:16 +0100

    Ejercicio 4

---
+++ space_invaders_mencion/Assets/Scripts/Enemy.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speedEnemy;
+    private State currentState = State.INIT;
+    public enum State
+    {
+        INIT,
+        RIGHT,
+        LEFT
+    }
+
+    public State state;
+
+    void Start()
+    {
+        state = State.RIGHT;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (state)
+        {
+            case State.INIT:
+                
+                break;
+            case State.RIGHT:
+                transform.Translate(Vector3.right * Time.deltaTime * speedEnemy);
+                OnChangeState(State.RIGHT);
+                break;
+            case State.LEFT:
+                transform.Translate(Vector3.left * Time.deltaTime * speedEnemy);
+                OnChangeState(State.LEFT);
+                break;
+        }
+
+        if (transform.position.x <= -6.7f)
+        {
+            state = State.LEFT;
+        }
+
+        if (transform.position.x >= 6.7f)
+        {
+            state = State.RIGHT;
+        }
+    }
+
+    private void OnChangeState(State state)
+    {
+        if(currentState != state)
+        {
+            speedEnemy += 0.5f;
+            currentState = state;
+        }
+    }
+}
---
+++ space_invaders_mencion/Assets/Scripts/SpaceShipScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShipScript : MonoBehaviour
+{
+    public int speed;
+    private Rigidbody2D rigid;
+
+    public GameObject torpedo;
+    public float speedTorpedo;
+
+    public Transform initPositionTorpedo;
+
+    public int totalBulletsInScene = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float movement = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+
+        rigid.velocity = transform.right * movement * speed;
+
+        float xPos = Mathf.Clamp(transform.position.x, -6f, 6f);
+
+        transform.position = new Vector2(xPos, transform.position.y);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            if (totalBulletsInScene == 0)
+            {
+                totalBulletsInScene++;
+                GameObject newTorpedo = Instantiate(torpedo, initPositionTorpedo.transform.position, Quaternion.identity);
+                newTorpedo.name = "Torpedo";
+                newTorpedo.tag = "Torpedo";
+                StartCoroutine(destroyBullet(newTorpedo));
+            }
+        }
+    }
+
+    IEnumerator destroyBullet(GameObject bullet)
+    {
+        yield return new WaitForSeconds(3.5f);
+        Destroy(bullet);
+        //totalBulletsInScene = 0;
+    }
+}
+
+
